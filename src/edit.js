d30279c4b3912fdc4055031bb425d32ffa486e46
kondo tomonori <tomonorik1001@gmail.com>
2021-07-14 22:48:03 +0900

    🔧

--- src/edit.js
+++ src/edit.js
@@ -34,97 +34,97 @@ import "./editor.scss";
 import $ from "jquery";
 
 export default function Edit({ attributes, setAttributes }) {
-	const blockProps = useBlockProps();
-	const { apiKey, apiStatus, placeId, res } = attributes;
-	const [apiTrue, setApiTrue] = useState(false);
+  const blockProps = useBlockProps();
+  const { apiKey, apiStatus, placeId, res } = attributes;
+  const [apiTrue, setApiTrue] = useState(false);
 
-	console.log(apiKey,res,apiStatus,placeId);
-	const clickEvent = () => {
-		$.getScript(
-			"https://maps.google.com/maps/api/js?key=" + apiKey + "&libraries=places",
-			function () {
-				const service = new google.maps.places.PlacesService(
-					document.createElement("div")
-					);
-					service.getDetails(
-						{
-							placeId: placeId,
-							fields: ["review"],
-						},
-					function (place, status) {
-						console.log(status);
-						if (status === "INVALID_REQUEST"){
-							attributes.apiStatus = "false";
-							attributes.res = '';
-							setApiTrue(false);
+  console.log(apiKey,res,apiStatus,placeId);
+  const clickEvent = () => {
+    $.getScript(
+      "https://maps.google.com/maps/api/js?key=" + apiKey + "&libraries=places",
+      function () {
+        const service = new google.maps.places.PlacesService(
+          document.createElement("div")
+          );
+          service.getDetails(
+            {
+              placeId: placeId,
+              fields: ["review"],
+            },
+          function (place, status) {
+            console.log(status);
+            if (status === "INVALID_REQUEST"){
+              attributes.apiStatus = "false";
+              attributes.res = '';
+              setApiTrue(false);
 
-							console.log('error');
-						}
-						if (status == google.maps.places.PlacesServiceStatus.OK) {
-							// 口コミURL
-							for(let i = 0; i < place.reviews.length; i ++){
-								const originUrl = place.reviews[i].author_url;
-								const originUrlArray =originUrl.split('/');
-								originUrlArray.pop();
-								originUrlArray.push('place');
-								originUrlArray.push(placeId);
-								const reviewUrl = originUrlArray.join('/');
-								place.reviews[i].reviewUrl = reviewUrl;
-							}
-							console.log(place.reviews);
+              console.log('error');
+            }
+            if (status == google.maps.places.PlacesServiceStatus.OK) {
+              // 口コミURL
+              for(let i = 0; i < place.reviews.length; i ++){
+                const originUrl = place.reviews[i].author_url;
+                const originUrlArray =originUrl.split('/');
+                originUrlArray.pop();
+                originUrlArray.push('place');
+                originUrlArray.push(placeId);
+                const reviewUrl = originUrlArray.join('/');
+                place.reviews[i].reviewUrl = reviewUrl;
+              }
+              console.log(place.reviews);
 
-							attributes.res = place.reviews;
-							attributes.apiStatus = "true";
-							setApiTrue(true);
+              attributes.res = place.reviews;
+              attributes.apiStatus = "true";
+              setApiTrue(true);
 
-							
-						}
-					}
-				);
-			}
-		);
-	};
+              
+            }
+          }
+        );
+      }
+    );
+  };
 
-	return (
-		<>
-			<InspectorControls>
-				<PanelBody title="API情報入力">
-					<TextControl
-						label="APIキー"
-						value={attributes.apiKey}
-						onChange={(value) => setAttributes({ apiKey: value })}
-					/>
-					<p>APIキーの取得は<a href="https://developers.google.com/maps?hl=ja">こちら</a></p>
-					<p>※APIキーはHTTPリファラー等で制限を行ってください。詳細は<a href="https://developers.google.com/maps/api-key-best-practices?hl=ja">こちら</a></p>
-					<TextControl
-						label="ロケーションID"
-						value={attributes.placeId}
-						onChange={(value) => setAttributes({ placeId: value })}
-					/>
-					<p>PlacesIDの検索は<a href="https://developers.google.com/maps/documentation/places/web-service/place-id">こちら</a></p>
-					<button onClick={clickEvent}>口コミ情報を表示する</button>
-					{apiStatus === "false" && (
-						<p>口コミ情報の取得に失敗しました。正しく情報が入力されているかご確認ください。</p>
-					)}
-				</PanelBody>
-			</InspectorControls>
-			<div {...blockProps}>
-				<div className="review">
-					{apiStatus === "true" && res !== "" && res.map((value, key) => {
-							return (
-								<div className="review__item" key={`review-${key}`}>
-									<div className="review__user">
-										<img src={value.profile_photo_url}/>
-										<p>{value.author_name}</p>
-									</div>
-									<p className="review_rating" data-rating={value.rating}>星の数：{value.rating}</p>
-									<a href={value.reviewUrl} target="_blank" rel="noreferrer noopener" className="review__text">{value.text}</a>
-								</div>
-							)
-						})
-					}
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <InspectorControls>
+        <PanelBody title="API情報入力">
+          <TextControl
+            label="APIキー"
+            value={attributes.apiKey}
+            onChange={(value) => setAttributes({ apiKey: value })}
+          />
+          <p>APIキーの取得は<a href="https://developers.google.com/maps?hl=ja">こちら</a></p>
+          <p>※APIキーはHTTPリファラー等で制限を行ってください。詳細は<a href="https://developers.google.com/maps/api-key-best-practices?hl=ja">こちら</a></p>
+          <TextControl
+            label="ロケーションID"
+            value={attributes.placeId}
+            onChange={(value) => setAttributes({ placeId: value })}
+          />
+          <p>PlacesIDの検索は<a href="https://developers.google.com/maps/documentation/places/web-service/place-id">こちら</a></p>
+          <button onClick={clickEvent}>口コミ情報を表示する</button>
+          {apiStatus === "false" && (
+            <p>口コミ情報の取得に失敗しました。正しく情報が入力されているかご確認ください。</p>
+          )}
+        </PanelBody>
+      </InspectorControls>
+      <div {...blockProps}>
+        <div className="review">
+          {apiStatus === "true" && res !== "" && res.map((value, key) => {
+              return (
+                <div className="review__item" key={`review-${key}`}>
+                  <div className="review__user">
+                    <img src={value.profile_photo_url}/>
+                    <p>{value.author_name}</p>
+                  </div>
+                  <p className="review_rating" data-rating={value.rating}>星の数：{value.rating}</p>
+                  <a href={value.reviewUrl} target="_blank" rel="noreferrer noopener" className="review__text">{value.text}</a>
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+    </>
+  );
 }
